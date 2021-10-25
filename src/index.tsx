a9b78677ea01ca209456be31cf2c00cf7ebe2815
pdimarcodev <pablo.d.dimarco@gmail.com>
2021-10-25 11:26:18 -0300

    minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import Navigator from "./config/Navigator";
+import Navigator from "@config/Navigator";
 import { AppBootstrap } from "@components";
 
 export default function App(): ReactElement {
